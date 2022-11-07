04149bb4704396a75908157ec6d27ace957cd22b
mramadhanurh <mramadhanur@gmail.com>
2022-11-07 13:55:30 +0700

    interface homepage

---
+++ lib/pages/category_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Center());
+  }
+}
